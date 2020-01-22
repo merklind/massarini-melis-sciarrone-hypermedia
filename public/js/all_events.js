c03b4babf2a0c4eaf73855ef2b358eea2f6454a0
marcomelis <marco.melis@mail.polimi.it>
2020-01-22 17:58:51 +0100

    fix some bug

--- public/js/all_events.js
+++ public/js/all_events.js
@@ -8,19 +8,19 @@ $(document).ready(function() {
         data.reverse().forEach((event) => {
             var template = `
                 <div class='row one_event' id='${event.id}'>
-                    <div class='imgcol'>
-                        <img src='images/${event.id}.jpg' alt='' class='image-event'>
+                    <div class='col-sm-3'>
+                        <img src='images/${event.id}.jpg' alt='ciao' class='image-event img-fluid'>
                     </div>
-                    <div class='abstract-information'>
+                    <div class='col-sm-9 abstract-information'>
                         Name: <span class='name-event'>${event.name}</span>
                         <br>
                         Date: <span class='date-event'>${event.date}</span>
                         <br>
                         Genre: <span class='genre-event'></span>
                         <br>
-                        Description: <span class='description-event'>${event.description}</span>
+                        Description: <span class='description-event'>${event.description.substring(0, 200)}...</span>
                         <br>
-                        <a class="btn btn-primary" href="one_event.html" role="button">Go to event</a>
+                        <a class="btn btn-primary" href="one_event.html/?=${event.id}" role="button">Go to event</a>
                     </div>
                 </div>`;
             $('#wrapper').prepend(template)
