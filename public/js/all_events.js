6ac18c1c28eeb9ed96a13f6e7b6c300c20fa6e18
marcomelis <marco.melis@mail.polimi.it>
2020-01-23 16:23:37 +0100

    Modified contact_us page and fix some bug in others page

--- public/js/all_events.js
+++ public/js/all_events.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $.ajax({
         method: 'GET',
-        url: 'http://mbmfestival.herokuapp.com/artisticEvents',
-
+        url: 'http://mbmfestival.herokuapp.com/artisticEvents'
     })
     .then(function(data) {
+        console.log(data)
         data.reverse().forEach((event) => {
             var template = `
                 <div class='row one_event' id='${event.id}'>
@@ -16,11 +16,11 @@ $(document).ready(function() {
                         <br>
                         Date: <span class='date-event'>${event.date}</span>
                         <br>
-                        Genre: <span class='genre-event'></span>
+                        Genre: <span class='genre-event'>${event.genre}</span>
                         <br>
                         Description: <span class='description-event'>${event.description.substring(0, 200)}...</span>
                         <br>
-                        <a class="btn btn-primary" href="one_event.html?=${event.id}" role="button">Go to event</a>
+                        <a class="btn btn-primary" href="one_event.html?id=${event.id}" role="button">Go to event</a>
                     </div>
                 </div>`;
             $('#wrapper').prepend(template)
--- public/js/specific_event.js
+++ public/js/specific_event.js
@@ -1,3 +1,12 @@
-$('document').ready(function() {
-    console.log("Pagina pronta")
+$(document).ready(function() {
+    var currentURL_string = window.location.href
+    var currentURL = new URL(currentURL_string)
+    var id = currentURL.searchParams.get('id')
+    $.ajax({
+        method:'GET',
+        url: 'http://mbmfestival.herokuapp.com/artisticEvents/' + id,
+        success: function(data) {
+            
+        }
+    })
 })
